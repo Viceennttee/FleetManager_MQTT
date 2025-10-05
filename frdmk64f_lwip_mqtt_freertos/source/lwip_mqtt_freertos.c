0929553c3664b9c04b6724975149bb721d96892a
cobiansm <sandra_smca@hotmail.com>
2025-10-05 01:29:28 -0600

    low_level_controller publishers and subscribers

--- frdmk64f_lwip_mqtt_freertos/source/lwip_mqtt_freertos.c
+++ frdmk64f_lwip_mqtt_freertos/source/lwip_mqtt_freertos.c
@@ -116,12 +116,6 @@ static char mqtt_payload_buf[MQTT_MAX_PAYLOAD];
 typedef enum {
 	speed_slider = 0,
 	battery_entry,
-	laser_slider,
-	bumper_button,
-	mode_selector,
-	switch_connection,
-	option_list,
-	battery_status,
 	motion_cmd,
     //insert new topics, and change current ones
     TOPIC_COUNT
@@ -135,12 +129,6 @@ typedef union
 	{
 		uint8_t 	speed_slider 		: 	1;
 		uint8_t		battery_entry 		: 	1;
-		uint8_t		laser_slider  		:	1;
-		uint8_t		bumper_button 		:	1;
-		uint8_t		mode_selector 		:	1;
-		uint8_t		switch_connection 	:	1;
-		uint8_t		option_list 		:	1;
-		uint8_t		battery_status		: 	1;
 		uint8_t		motion_cmd			: 	1;
 		uint8_t		reserved	 		:	1;
 	}topics;
@@ -156,12 +144,6 @@ static const struct {
     //change names and add topics
     {"/speed_slider/data/value", speed_slider},
     {"/battery_entry/data/value", battery_entry},
-	{"/laser_slider/data/value" ,  laser_slider},
-	{"/bumper_button/pressed/value", bumper_button},
-	{"/mode_selector/drive_mode/value", mode_selector},
-	{"/switch_connection/state", switch_connection},
-	{"/option_list/job", option_list},
-	{"/low-level/battery/percentage", battery_status},
 	{"/core/sensor_laser/data/value", motion_cmd},
     {NULL, TOPIC_COUNT}
 };
@@ -179,12 +161,6 @@ static const struct
 		 //change names and add topics
 		{"/low_level_controller/speed/data/value", speed_slider},
 		{"/low_level_controller/battery/percentage",battery_entry},
-		{"/core/sensor_laser/data/value", laser_slider},
-		{"/core/sensor_bumper/data", bumper_button},
-		{"/core/driving_mode/mode", mode_selector},
-		{"/fleet/connection_status/state",switch_connection, },
-		{"/fleet/robot_status/state", option_list},
-		{"/fleet/battery_status/status", battery_status},
 		{"/low_level_controller/motion/command", motion_cmd},
 		{NULL, TOPIC_COUNT}
 		};
@@ -272,20 +248,8 @@ static void mqtt_incoming_publish_cb(void *arg, const char *topic, u32_t tot_len
         	topic_status.topics.speed_slider = 1;
         else if (strcmp(topic, topic_map[battery_entry].topic_name) == 0)
         	topic_status.topics.battery_entry = 1;
-        else if (strcmp(topic, topic_map[laser_slider].topic_name) == 0)
-        	topic_status.topics.laser_slider = 1;
-        else if (strcmp(topic, topic_map[bumper_button].topic_name) == 0)
-        	topic_status.topics.bumper_button = 1;
-        else if (strcmp(topic, topic_map[mode_selector].topic_name) == 0)
-        	topic_status.topics.mode_selector = 1;
-        else if (strcmp(topic, topic_map[switch_connection].topic_name) == 0)
-        	topic_status.topics.switch_connection = 1;
-        else if (strcmp(topic, topic_map[option_list].topic_name) == 0)
-        	topic_status.topics.option_list = 1;
-        else if (strcmp(topic, topic_map[battery_status].topic_name) == 0)
-        	topic_status.topics.option_list = 1;
         else if (strcmp(topic, topic_map[motion_cmd].topic_name) == 0)
-        	topic_status.topics.option_list = 1;
+        	topic_status.topics.motion_cmd = 1;
         else
         	PRINTF("Unknown topic");
 
@@ -316,31 +280,6 @@ static void mqtt_incoming_data_cb(void *arg, const u8_t *data, u16_t len, u8_t f
 	{
 		/*Laser in proximity */
 		to_publish = mqtt_payload_buf;
-	}else if (topic_status.topics.laser_slider)
-    {
-    	/*Laser in cm */
-    	to_publish = mqtt_payload_buf;
-    }else if (topic_status.topics.bumper_button)
-    {
-    	/* bumper TRUE/False */
-    	to_publish = mqtt_payload_buf;
-	}else if (topic_status.topics.mode_selector)
-	{
-		/*Auto Manual button*/
-    	if (strcmp("Mode Manual",data )==0)
-    		GPIO_PortToggle(BOARD_LED_GPIO, 1u << BOARD_LED_GPIO_PIN);
-	}else if (topic_status.topics.switch_connection)
-    {
-    	/* switch for robot connection */
-    	to_publish = mqtt_payload_buf;
-	}else if (topic_status.topics.option_list)
-    {
-    	/* list for current job */
-    	to_publish = mqtt_payload_buf;
-	}else if (topic_status.topics.battery_status)
-    {
-    	/* battery status lcd */
-    	to_publish = mqtt_payload_buf;
 	}
     if (flags & MQTT_DATA_FLAG_LAST) {
 		err_t err = tcpip_callback(publish_message, NULL);
@@ -479,25 +418,8 @@ static void publish_message(void *ctx)
 	topic = publish_topic_map[speed_slider].topic_publish_name;
  else if (topic_status.topics.battery_entry)
  	topic = publish_topic_map[battery_entry].topic_publish_name;
- else if (topic_status.topics.laser_slider)
-	topic = publish_topic_map[laser_slider].topic_publish_name;
- else if (topic_status.topics.bumper_button)
-	topic = publish_topic_map[bumper_button].topic_publish_name;
  else if (topic_status.topics.motion_cmd)
     topic = publish_topic_map[motion_cmd].topic_publish_name;
- else if (topic_status.topics.mode_selector)
-    topic = publish_topic_map[mode_selector].topic_publish_name;
- else if (topic_status.topics.switch_connection)
-     topic = publish_topic_map[switch_connection].topic_publish_name;
- else if (topic_status.topics.option_list)
-      topic = publish_topic_map[option_list].topic_publish_name;
- else if (topic_status.topics.battery_status)
-      topic = publish_topic_map[battery_status].topic_publish_name;
-
-
-
-  // static const char *topic2   = "bar";
- //   static const char *message2 = "test msg from bar";
 
     LWIP_UNUSED_ARG(ctx);
 
